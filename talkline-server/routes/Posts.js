1e2bdd46e138c71acbdfcba09f290220dd1d1d65
belladev250 <habanathomas1982@gmail.com>
2022-03-13 20:15:12 +0200

    finalize backende

--- talkline-server/routes/Posts.js
+++ talkline-server/routes/Posts.js
@@ -18,7 +18,7 @@ try{
 
 //update a post 
 router.put('/:id',async(req,res)=>{
-   try{
+   try{ 
 
     const post = await postModel.findById(req.params.id)
     if(post.userId === req.body.userId){
@@ -48,10 +48,49 @@ router.delete('/:id',async(req,res)=>{
     }
 })
 
+// get a post
 
+router.get('/:id',async(req,res)=>{
+    try{
+        const getPost = await postModel.findById(req.params.id)
+        res.status(200).json(getPost)
+    }catch(err){
+        res.status(500).send(err)
+    }
 
+})
 
+// like or dislike a post
+router.put('/:id/like',async(req,res)=>{
+    try{
+        const post = await  postModel.findById(req.params.id)
+        if(!post.likes.includes(req.body.userId)){
+           await post.updateOne({$push:{likes:req.body.userId}})
+           res.status(200).send("post has been liked")
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}})
+          res.status(200).send("post has been disliked")
+        }
+    }catch(err){
+        res.status(500).send(err)
+    }
+})
 
-
+//get timeline posts
+router.get('/timeline/all',async(req,res)=>{
+       
+    try{
+         const currentUser = await User.findById(req.body.userId)
+         const userPosts = await postModel.find({userId:currentUser._id})
+         const friendPosts = await Promise.all(
+             currentUser.following.map((friendId)=>{
+                 return postModel.find({userId:friendId})
+             })
+         );
+         res.json(userPosts.concat(...friendPosts))
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports=router
